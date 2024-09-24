f17b37471238336f12bf0c3f22e977cad1a3df40
vlemalieu <vlemalieu@stud.hs-bremen.de>
2024-09-24 09:34:34 +0200

    Struktur anlegen,NavigationBar erstellen

---
+++ ti-folienzusammenfassung/src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import NavigationBar from './components/NavigationBar';
+
+function App() {
+  return (
+    <div>
+      <NavigationBar />
+      {/* Other components will go here */}
+    </div>
+  );
+}
+
+export default App;
---
+++ ti-folienzusammenfassung/src/components/NavigationBar.js
@@ -0,0 +1,64 @@
+// NavigationBar.js
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+const NavigationBar = () => {
+  return (
+    <AppBar
+      position="static"
+      sx={{
+        backgroundColor: '#E5E2DA', // light beige/grey color as seen in the screenshot
+        boxShadow: 'none', // Remove shadow
+      }}
+    >
+      <Toolbar sx={{ justifyContent: 'space-between' }}>
+        {/* Logo/Title */}
+        <Typography
+          variant="h6"
+          sx={{
+            flexGrow: 1,
+            fontWeight: 'bold',
+            fontSize: '1.5rem',
+            color: '#121C2C', // Dark blue color for the text
+          }}
+        >
+          SmartSummaries
+        </Typography>
+
+        {/* Navigation Links */}
+        <Button
+          sx={{
+            color: '#121C2C', // Same dark blue color for buttons
+            textTransform: 'none', // Keep text case as in original
+            fontSize: '1.1rem',
+          }}
+        >
+          Home
+        </Button>
+        <Button
+          sx={{
+            color: '#121C2C',
+            textTransform: 'none',
+            fontSize: '1.1rem',
+          }}
+        >
+          Services
+        </Button>
+        <Button
+          sx={{
+            color: '#121C2C',
+            textTransform: 'none',
+            fontSize: '1.1rem',
+          }}
+        >
+          Kontakt
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default NavigationBar;
